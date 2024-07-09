9a848900b978466200126fd647e75c5a24f6d374
khutyu <2020385155@ufs4life.ac.za>
2024-07-09 13:53:14 +0200

    Adds initial Design of the Project

---
+++ MarketPlace/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using MarketPlace.Models;
+namespace MarketPlace
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        public string BuyerUserName { get; set; }
+        public string SellerUserName { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalPrice { get; set; }
+
+       
+        public IEnumerable<Product> Products { get; set; }
+        
+    }
+}
